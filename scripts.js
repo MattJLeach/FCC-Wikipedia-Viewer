c8b1d7b51e20d65ab42b81f843e28dcfe3e9d6e8
Matt Leach <email@mattleach.info>
2016-10-16 00:12:50 +0100

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(function() {
+	
+	$('#search').bind("enterKey", function(e) {
+		
+		var searchWord = $('#search').val();
+
+		$.ajax({
+			url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchWord + "&format=json",
+			type: "GET",
+			dataType: "jsonp",
+			success: function(data) {
+				// Code for successful AJAX call
+
+				console.log(data[1]);
+
+				for( var i = 0; i < data[1].length; i++) {
+					$('#result').append('<a href="#"><div class="result-item"><h3>' + data[1][i] + '</h3><p>' + data[2][i] + '</div></a>');
+				}
+
+			},
+			error: function(err) {
+				alert("Error");
+			}
+		});
+
+	});
+	$('#search').keyup(function(e) {
+		if(e.keyCode == 13) {
+			$(this).trigger("enterKey");
+		}
+	});
+
+
+
+});
